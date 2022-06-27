c669f7cd6865414f53bbaab53f0ced2f0178fc40
Aji Codi <codiaji@gmail.com>
2022-06-27 14:54:23 +0100

    first working solution

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', (event) => {
-  let clickTime = Date.now();
-  console.log(clickTime);
   const { target } = event;
+  console.log('Clicked');
   target.style['pointer-events'] = 'none';
   target.disabled = true;
-  while (Date.now() - clickTime <= 10000) {}
-  target.disabled = false;
-  target.style['pointer-events'] = 'all';
+  setTimeout(() => {
+    target.disabled = false;
+    target.style['pointer-events'] = 'all';
+  }, 1000);
 });
